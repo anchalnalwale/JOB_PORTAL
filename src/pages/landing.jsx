7327fadd697806763ec55ef9f22afa8675150cde
anchalnalwale <nalwaleanchal@gmail.com>
2025-12-31 02:10:55 +0530

    Added data

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Button } from '../components/ui/button'
 import { Link } from 'react-router-dom'
+import { CarouselItem } from '@/components/ui/carousel'
+import companies from '../data/companies.json'
+import faq from '../data/faq.json'
+import { Carousel } from '@/components/ui/carousel'
+import { CarouselContent } from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 
 const LandingPage = () => {
   return (
@@ -9,7 +17,7 @@ const LandingPage = () => {
         <h1 className='flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4'>Find Your Dream Job{" "}
           <span className='flex items-center gap-2 sm:gap-6'>
             and get{" "}
-            <img src="/logo.png" alt="Hirrd Logo" className='h-14 sm:h-24 lg:h-32'/>
+            <img src="/logo.png" alt="Hirrd Logo" className='h-14 sm:h-24 lg:h-32' />
           </span>
         </h1>
         <p className='text-gray-300 sm:mt-4 text-xs sm:text-xl'>
@@ -26,14 +34,50 @@ const LandingPage = () => {
         </Link>
       </div>
       {/* carousel */}
+      <Carousel
+        plugins={[Autoplay({ delay: 2000 })]}
+        className="w-full py-10">
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+                <img src={path} alt={name} className='h-9 sm:h-14 object-contain' />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
 
       {/* banner */}
-
-      <section>
-        {/* cards */}
+      <img src="/banner.jpeg" className='w-full' />
+      <section className='grid grid-cols-1 md:grid-cols-2 gap-4 justify-center'>
+        <Card>
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Search and applyfor jobs, track applications, and more.</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Post jobs, manage applications, and find the best candidates.</p>
+          </CardContent>
+        </Card>
       </section>
 
       {/* Accordion */}
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </main>
   )
 }
