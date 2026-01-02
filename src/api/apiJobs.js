861aec8d603ab14bd4fbe18b46196ba4e3e40686
anchalnalwale <nalwaleanchal@gmail.com>
2026-01-02 23:55:03 +0530

    Done changes

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,15 +1,28 @@
-import supabase from "@/utlis/supabase";
-import { SupabaseClient } from "@supabase/supabase-js";
+import supabaseClient from "@/utils/supabase";
 
-export async function getJobs(token) {
-    const supabase = await SupabaseClient(token);
+export async function getJobs(token, { location, company_id, searchQuery }) {
+    const supabase = await supabaseClient(token);
 
-    let query = supabase.from("jobs").select("*");
+    let query = supabase
+        .from("jobs")
+        .select("*, company:companies(name,logo_url),saved_jobs(id)");
 
-    const {data, error} = await query;
+    if (location) {
+        query = query.eq("location", location);
+    }
 
-    if(error) {
-        console.log("Error fetching Jobs:", error);
+    if (company_id) {
+        query = query.eq("company_id", company_id);
+    }
+
+    if (searchQuery) {
+        query = query.ilike("title", `%${searchQuery}%`);
+    }
+
+    const { data, error } = await query;
+
+    if (error) {
+        console.error("Error fetching Jobs:", error);
         return null;
     }
 
--- src/hooks/use-fetch.jsx
+++ src/hooks/use-fetch.jsx
@@ -1,32 +1,32 @@
 import { useSession } from "@clerk/clerk-react";
 import { useState } from "react";
 
-const useFetch = (cb, options = {}) =>{
+const useFetch = (cb, options = {}) => {
     const [data, setData] = useState(undefined);
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
 
-    const {session} = useSession();
+    const { session } = useSession();
 
-    const fn = async(...args) =>{
+    const fn = async (...args) => {
         setLoading(true);
         setError(null);
 
-        try{
+        try {
             const supabaseAccessToken = await session.getToken({
                 template: "supabase",
             });
 
-            const response = await cb(supabaseAccessToken,options, ...args);
+            const response = await cb(supabaseAccessToken, options, ...args);
             setData(response);
             setError(null);
-        }catch(error) {
+        } catch (error) {
             setError(error);
-        }finally {
+        } finally {
             setLoading(false);
         }
     };
-    return {fn, data, loading, error};
+    return { fn, data, loading, error };
 };
 
 export default useFetch;
